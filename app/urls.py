79f64d571e9b76fecbcd1b7cd9a21b299713100e
amekin <anmekin@gmail.com>
2015-03-03 18:03:50 +0600

    Fixed small bugs

--- app/urls.py
+++ app/urls.py
@@ -6,7 +6,7 @@ from views import *
 urlpatterns = patterns('',
                        url(r'^$', home, name='home'),
                        url(r'^test', recomended, name='recomended'),
-                       url(r'^photos', show_fotos, name='show_fotos'),
+                       url(r'^photos', show_photos, name='show_fotos'),
                        url(r'^logout', logout, name='logout'),
                        url(r'^auth$', auth, name='auth'),
                        )
