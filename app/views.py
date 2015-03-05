65b8c1639e25d22dcb771146e368be37b6ae9964
amekin <anmekin@gmail.com>
2015-03-05 19:39:01 +0600

    Fixed hardcoded nsid in oauth_callback

--- app/views.py
+++ app/views.py
@@ -9,13 +9,13 @@ from django.contrib.auth.decorators import login_required
 from search_algorithm import *
 from models import *
 from tasks import update_flickr_user
-
+from social.storage.django_orm import DjangoUserMixin
 import time
 import flickrapi
 
 def oauth_callback(request):
-    flickruser, created = FlickrUser.objects.get_or_create(nsid='130664317@N04')
-                                            # flickr nsid != request.user.username, should find it in user
+    flickruser, created = FlickrUser.objects.get_or_create(
+        nsid=DjangoUserMixin.get_social_auth_for_user(request.user)[0].uid)
     if created:
         flickruser.user = request.user
         flickruser.save()
