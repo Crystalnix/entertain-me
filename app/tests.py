98b0c7da7a86d761c1edc2849025f5a07b7575b7
amekin <anmekin@gmail.com>
2015-03-06 16:31:54 +0600

    Added unlogged tests

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+import nose.tools as nt
+from models import *
+import factory
+import random
+from factories import *
+import unittest
 
-# Create your tests here.
+
+class MyTestCase(TestCase):
+    def test_unlogged_oauth_callback(self):
+        c = Client()
+        response = c.get('/oauth_callback/')
+
+        self.assertContains(response, "Unknown Flickr account")
+
+    def test_unlogged_get_photo(self):
+        c = Client()
+        response = c.get('/get_photo/', follow=True)
+        print response.redirect_chain
+        self.assertRedirects(response, "/?next=/get_photo/")
