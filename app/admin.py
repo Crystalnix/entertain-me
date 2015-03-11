66d0818a2932e6c5595eb263f9fa7204ef497385
amekin <anmekin@gmail.com>
2015-03-11 19:00:15 +0600

    Changed model views in admin

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
+from django.shortcuts import render
 from models import *
+from django.contrib.admin.views.decorators import staff_member_required
+
 admin.site.register(FlickrUser)
 admin.site.register(Photo)
 admin.site.register(Liking)
 admin.site.register(Review)
 # Register your models here.
+
+@staff_member_required
+def photo_info(request, id):
+    photo = Photo.objects.get(id=id)
+    return render(request, 'admin_photo.html', {'photo': photo})
+
+@staff_member_required
+def flickruser_info(request, pk):
+    flickruser = FlickrUser.objects.get(pk=pk)
+    return render(request, 'admin_flickruser.html', {'user': flickruser})
