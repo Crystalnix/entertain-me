f3c0ffdacfca05ae3ca85dad949034d7030f5554
amekin <anmekin@gmail.com>
2015-03-12 16:01:12 +0600

    Admin views are returned

--- app/admin.py
+++ app/admin.py
@@ -8,13 +8,3 @@ admin.site.register(Photo)
 admin.site.register(Liking)
 admin.site.register(Review)
 # Register your models here.
-
-@staff_member_required
-def photo_info(request, id):
-    photo = Photo.objects.get(id=id)
-    return render(request, 'admin_photo.html', {'photo': photo})
-
-@staff_member_required
-def flickruser_info(request, pk):
-    flickruser = FlickrUser.objects.get(pk=pk)
-    return render(request, 'admin_flickruser.html', {'user': flickruser})
--- entertain_me/urls.py
+++ entertain_me/urls.py
@@ -5,8 +5,6 @@ from django.contrib import admin
 urlpatterns = patterns('',
     url('', include('social.apps.django_app.urls', namespace='social')),
     url(r'', include('app.urls')),
-    url(r'^admin/app/photo/(\d+)', 'app.admin.photo_info'),
-    url(r'^admin/app/flickruser/(\d+)', 'app.admin.flickruser_info'),
     url(r'^admin/', include(admin.site.urls)),
 )
 
